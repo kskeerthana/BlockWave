2e9bcc2edcc81e4f2a05f130e74c7c61030303b4
kskeerthana <keerks07@gmail.com>
2023-07-01 15:48:33 -0400

    BlockWave

---
+++ Wave/hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+task("balance", "Prints an account's balance").setAction(async () => {
+  console.log("checking task's working")
+});
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+      console.log(account.address);
+  }
+});
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      // This value will be replaced on runtime
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: process.env.PROD_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
---
+++ readme.md
@@ -0,0 +1,16 @@
+**Hi! This is my web3 wave project**
+
+When you wave at me, it is deployed and stored on a smart contract. 
+I've used Solidity, ReactJs for programming, and APIs/Tools I've used are QuickNode and Hardhat. 
+
+<div 
+    style="position: relative; padding-bottom: 62.5%; height: 0;">
+    <iframe 
+        src="https://www.loom.com/embed/23a365c101d34404a452298c45b8ea06?sid=4465018e-5068-4f46-8d0f-a2c2615dc2e8" 
+        frameborder="0" 
+        webkitallowfullscreen 
+        mozallowfullscreen 
+        allowfullscreen 
+        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;">
+    </iframe>
+</div>
